413398007e396a51dcb923df45457b6911ad13c4
demalu <lucadema98@gmail.com>
2023-08-22 15:57:26 +0200

    people level

--- Control_people_level.py
+++ Control_people_level.py
@@ -105,28 +105,31 @@ class lighting_control():
 
 if __name__ == "__main__":
     conf = json.load(open("Connector/settings.json"))  # File contenente broker, porta e basetopic
-    # Io mi devo connettere al catalog e ricavare building e room, sensorID, topic, measure, broker, port
-    Sensors = []
     baseTopic = conf["baseTopic"]
-    BuildingID = [str(i) for i in range(1)]
-    roomIDs = [f"{BuildingID[i]}_{i + 1}" for i in range(len(BuildingID))]
     broker = conf["broker"]
     port = conf["port"]
-    # I need clientID, baseTopic, buildingID, roomID, measure, broker, port, notifier, threshold
-    heating_control = lighting_control(controlID='Prova_Lighting',
-                                      baseTopic=baseTopic,
-                                      buildingID=BuildingID[0],
-                                      roomID=roomIDs[0],
-                                      sensorID=0,
-                                      measure='motion',
-                                      broker=broker,
-                                      port=port,
-                                      threshold=30.0)
-    heating_control.stop()
-    heating_control.start()
+
+    controls = json.load(open("actuators.json"))
+    heat_controls = []
+    for control in controls:
+        if control['measure_to_check'] == "motion":
+            heating_control = lighting_control(
+                baseTopic=baseTopic,
+                broker=broker,
+                port=port,
+                controlID=control['control_id'],
+                buildingID=control['building_id'],
+                roomID=control['room_id'],
+                sensorID=control['sensor_id'],
+                measure=control['measure_to_check'],
+                threshold=30.0)
+            heat_controls.append(heating_control)
+    for control in heat_controls:
+        control.stop()
+        control.start()
     a = 0
     while (a < 30):
         a += 1
         time.sleep(5)
-
-    heating_control.stop()
+    for control in heat_controls:
+        control.stop()
