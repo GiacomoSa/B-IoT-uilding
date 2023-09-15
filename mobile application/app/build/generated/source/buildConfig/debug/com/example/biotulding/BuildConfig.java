b226c9a5b29d83f3fef131b97d7762efc4eaa179
GiacomoSa <67115453+GiacomoSa@users.noreply.github.com>
2023-09-15 11:16:47 +0200

    app

---
+++ mobile application/app/build/generated/source/buildConfig/debug/com/example/biotulding/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.biotulding;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.biotulding";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
